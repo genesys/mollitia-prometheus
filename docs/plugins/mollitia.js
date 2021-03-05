9a489d13351232ef00b3ccb2609a423945531270
cadgerfeast <cadgerfeast@users.noreply.github.com>
2021-03-05 09:59:47 +0000

    deploy: 62f51c701157c956b8b3f022f9f2e2d891ea949f

--- docs/plugins/mollitia.js
+++
@@ -1,9 +0,0 @@
-import * as Mollitia from 'mollitia';
-import * as MollitiaPrometheus from './libs/mollitia-prometheus.es5';
-
-Mollitia.use(new MollitiaPrometheus.PrometheusAddon());
-
-export default (context, inject) => {
-  inject('mollitia', Mollitia);
-  inject('mollitiaPrometheus', MollitiaPrometheus);
-};
--- test/jest.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace jest {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface Matchers<R> {
-    toBeAround (expected: number, around: number): CustomMatcherResult;
-  }
-}
